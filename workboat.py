c67b732b485bdafe04b8e6d98ebbbe576fe23b8c
lehabimlubuntu <lehabimmarcial.ca@gmail.com>
2023-08-25 23:33:04 -0300

    Primera subida de codigo a GITHUB desde terminal de Lubuntu

---
+++ workboat.py
@@ -0,0 +1,8 @@
+# Project: Mariculture Farm
+# Videogame on pygame
+
+# COnstructing a game to simulate a guy that gets the mission to manage and
+# grow a mariculture bussiness (finfish, seaweed, mollusk and crustaceans)
+
+import pygame
+import sys
